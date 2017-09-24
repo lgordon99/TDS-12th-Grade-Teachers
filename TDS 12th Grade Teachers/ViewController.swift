b14cf5fb16ad2ed2adae88ae76e2fe03574df34f
lgordon99 <luciabeluga1@gmail.com>
2017-09-23 22:56:12 -0400

    Removed code from old version

--- TDS 12th Grade Teachers/ViewController.swift
+++ TDS 12th Grade Teachers/ViewController.swift
@@ -26,7 +26,6 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     let searchTF = UITextField()
     
     //MARK: Impermanent Variable Declarations
-    //var teachers: [String] = ["Ms. Ioppolo", "Mr. Moonga", "Ms. Rosenblatt", "Dr. Henry", "Ms. Spalding", "Mr. Tarbath"]
     var teachers: [String] = []
     var day = "A"
     var period = "1"
@@ -36,35 +35,24 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     var screenHeight: CGFloat = 0
     var csvData = ""
     var teacherCSV = ""
-    var csvRows = [[]]
-    
-    //MARK: Permanent Variable Declarations
-    let ioppolo: [String] = ["A1", "A3", "A5", "A6", "A7", "B1", "B2", "B4", "B5", "C2", "C3", "C5", "C6", "C7", "D3", "D5", "D6", "D7", "E2", "E3", "E5", "E7", "F1", "F2", "F3", "F5", "F6", "F7", "G1", "G5", "G6", "H1", "H2", "I1", "I3", "I4", "I5", "I7", "J3", "J4", "J5", "J6", "J7"]
-    let moonga: [String] = ["A2", "A3", "A4", "A5", "A6", "B1", "B5", "B6", "C1", "C2", "C3", "C5", "C7", "D1", "D2", "D3", "D4", "E2", "E3", "E4", "E6", "E7", "F1", "F3", "F6", "G1", "G2", "G3", "G4", "G5", "H1", "H2", "H3", "H5", "H6", "H7", "I3", "I6", "I7", "J2", "J4", "J6", "J7"]
-    let rosenblatt: [String] = ["A3", "A4", "A5", "A7", "B1", "B4", "B5", "B7", "C1", "C5", "C7", "D2", "D3", "D4", "D5", "D7", "E2", "E5", "E6", "E7", "F3", "F5", "F7", "G1", "G3", "G4", "G5", "H1", "H4", "H5", "H7", "I3", "I4", "I5", "I7", "J1", "J2", "J5", "J6", "J7"]
-    let henry: [String] = ["A1", "A2", "A3", "A5", "A6", "A7", "B1", "B3", "B6", "B7", "C2", "C5", "C6", "C7", "D1", "D3", "D4", "D5", "E2", "E4", "E5", "E7", "F1", "F3", "F4", "F5", "F6", "G1", "G2", "G4", "G5", "H1", "H3", "H4", "H5", "H6", "H7", "I2", "I3", "I5", "I6", "J1", "J3", "J5", "J6", "J7"]
-    let spalding: [String] = ["A2", "A3", "A4", "A5", "A6", "A7", "B1", "B6", "B7", "C1", "C2", "C7", "D1", "D2", "D3", "D4", "D5", "D7", "E4", "E5", "E6", "E7", "F1", "F3", "F5", "F7", "G1", "G2", "G3", "G4", "G5", "H3", "H4", "H5", "H6", "H7", "I3", "I5", "I6", "I7", "J1", "J2", "J3", "J5", "J7"]
-    let tarbath: [String] = ["A1", "A3", "A4", "A5", "A7", "B1", "B3", "B4", "B5", "B7", "C4", "C5", "C6", "C7", "D2", "D3", "D5", "D7", "E1", "E2", "E3", "E5", "E7", "F3", "F4", "F5", "F6", "F7", "G1", "G3", "G7", "H1", "H2", "H4", "H7", "I2", "I3", "I4", "I5", "I7", "J1", "J5", "J6", "J7"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         csvData = readDataFromCSV(fileName: "Teacher Schedules", fileType: "csv")
         csvData = cleanRows(file: csvData)
-        //csvRows = csv(data: csvData)
-        //print(csvRows)
-        //print(csvData)
         
         teacherCSV = readDataFromCSV(fileName: "Teacher Names", fileType: "csv")
         teacherCSV = cleanRows(file: teacherCSV)
+        
         while teacherCSV.contains("\"") {
             teacherCSV.remove(at: teacherCSV.index(of: "\"")!)
         }
+        
         while let rangeToReplace = teacherCSV.range(of: "\n") {
             teacherCSV.replaceSubrange(rangeToReplace, with: " ")
         }
-        //print(teacherCSV)
-        //var teacherArray = String(describing: teacherCSV.split(separator: " "))
+        
         var teacherArray = teacherCSV.components(separatedBy: " ")
         
         var i = 0
@@ -75,7 +63,6 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
                 i += 1
             }
         }
-        //print(teacherArray)
         
         for teacher in teacherArray {
             if teacher.contains(",") {
@@ -86,8 +73,6 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         }
         
         teachers = teacherArray
-        
-        //print(teacherArray)
         
         screenWidth = view.frame.size.width
         screenHeight = view.frame.size.height
@@ -314,33 +299,21 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         
         var teacherLN = ""
         var startPos = 0
-        var endPos = 0
         
         teacherLN = teacher
-        print(teacherLN)
  
         if let range = csvData.range(of: teacherLN){
              startPos = csvData.distance(from: csvData.startIndex, to: range.lowerBound)
-             endPos = csvData.distance(from: csvData.startIndex, to: range.upperBound)
         }
         
         let startIndex = csvData.index(csvData.startIndex, offsetBy: startPos)
         let endIndex = csvData.index(csvData.startIndex, offsetBy: startPos + teacherLN.characters.count + 250)
         let range = startIndex..<endIndex
-        
         let teacherData = String(csvData.substring(with: range))!
-        //print(teacherData)
-
         let aIndex = teacherData.distance(from: teacherData.startIndex, to: teacherData.index(of: "A")!)
-        //print(aIndex)
-        
         let justSchedule = String(teacherData.dropFirst(aIndex))
-        //print(justSchedule)
-        
         let quoteIndex = justSchedule.distance(from: justSchedule.startIndex, to: justSchedule.index(of: "\"")!)
         let withoutLetters = String(justSchedule.dropFirst(quoteIndex))
-        //print(withoutLetters)
-        
         let free = getFree(schedule: withoutLetters, day: day, period: period)
         
         if free {
@@ -353,6 +326,7 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     func getFree(schedule: String, day: String, period: String) -> Bool{
         var indexes: [Int] = []
         var searchRange = schedule.startIndex..<schedule.endIndex
+        
         let lettersToNumbers: [String: Int] = ["A": 1, "B": 2, "C": 3, "D": 4, "E": 5, "F": 6, "G": 7, "H": 8, "I": 9, "J": 10]
         let dayNumber = lettersToNumbers[day]
         let periodToNumber: [String: Int] = ["1": 1, "2": 2, "3": 3, "4": 4, "5a": 5, "5b": 6, "6": 7, "7": 8]
@@ -366,12 +340,8 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         
         let commaNumber = 10 * (periodNumber! - 1) + (dayNumber! - 1)
         let charIndex = indexes[commaNumber]
-        //print(indexes)
-        //print(commaNumber)
-        //print(charIndex)
         let ind = schedule.index(schedule.startIndex, offsetBy: charIndex + 1)
         let char = String(schedule[ind])
-        //print("char = " + char)
         
         if char == "," {
             return true
@@ -380,80 +350,6 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         }
     }
     
-    func updateText2(){
-        availabilityLabel.isHidden = false
-        availabilityLabel.text = ""
-        time = day + period
-        
-        if teacher == "Ms. Ioppolo" {
-            for item in ioppolo {
-                if time == item {
-                    availabilityLabel.text = "\(teacher) is available to meet \(time)!"
-                    break
-                } else {
-                    availabilityLabel.text = "\(teacher) is not available \(time)."
-                }
-            }
-        } else if teacher == "Mr. Moonga" {
-            for item in moonga {
-                if time == item {
-                    availabilityLabel.text = "\(teacher) is available to meet \(time)!"
-                    break
-                } else {
-                    availabilityLabel.text = "\(teacher) is not available \(time)."
-                }
-            }
-        } else if teacher == "Ms. Spalding" {
-            for item in spalding {
-                if time == item {
-                    availabilityLabel.text = "\(teacher) is available to meet \(time)!"
-                    break
-                } else {
-                    availabilityLabel.text = "\(teacher) is not available \(time)."
-                }
-            }
-        } else if teacher == "Ms. Rosenblatt" {
-            for item in rosenblatt {
-                if time == item {
-                    availabilityLabel.text = "\(teacher) is available to meet \(time)!"
-                    break
-                } else {
-                    availabilityLabel.text = "\(teacher) is not available \(time)."
-                }
-            }
-        } else if teacher == "Dr. Henry" {
-            for item in henry {
-                if time == item {
-                    availabilityLabel.text = "\(teacher) is available to meet \(time)!"
-                    break
-                } else {
-                    availabilityLabel.text = "\(teacher) is not available \(time)."
-                }
-            }
-        } else if teacher == "Ms. Spalding" {
-            for item in spalding {
-                if time == item {
-                    availabilityLabel.text = "\(teacher) is available to meet \(time)!"
-                    break
-                } else {
-                    availabilityLabel.text = "\(teacher) is not available \(time)."
-                }
-            }
-        } else if teacher == "Mr. Tarbath" {
-            for item in tarbath {
-                if time == item {
-                    availabilityLabel.text = "\(teacher) is available to meet \(time)!"
-                    
-                    break
-                } else {
-                    availabilityLabel.text = "\(teacher) is not available \(time)."
-                }
-            }
-        }
-    }
-    
-    
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
